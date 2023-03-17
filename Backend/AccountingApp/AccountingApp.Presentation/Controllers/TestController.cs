48404dcce84d1d8e5a88e793960be47ecd8cbe8d
seyyitatim <seyyitatim@gmail.com>
2023-03-17 16:19:16 +0300

    temel proje yapısı oluşturuldu

---
+++ Backend/AccountingApp/AccountingApp.Presentation/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using AccountingApp.Presentation.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Presentation.Controllers
+{
+    public sealed class TestController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("işlem başarılı");
+        }
+    }
+}
